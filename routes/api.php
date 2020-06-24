a62911400c59d509dc0980bc6217451dffca3ad4
adibest <ahmadjayadibya@gmail.com>
2020-06-24 20:25:20 +0700

    coba terus

--- routes/api.php
+++ routes/api.php
@@ -18,19 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return auth()->user();
 });
 
-Route::post('login', 'API\UserController@login');
-
-Route::post('register', 'API\UserController@register');
-
-// Route::group(['middleware' => ['cors', 'json.response']], function () {
-//     // ...
-//     // public routes
-//     Route::post('/login', 'Auth\ApiAuthController@login')->name('login.api');
-//     Route::post('/register','Auth\ApiAuthController@register')->name('register.api');
-//     Route::post('/logout', 'Auth\ApiAuthController@logout')->name('logout.api');
-//
-// });
-
 // List all posts
 Route::get('posts', 'PostController@index');
 // List a single post
